469276e7a7f9eb5da4039c5adc2ec83c1e432141
CEDRIC LEONARD <me@cedricleonard.be>
2018-01-26 16:59:16 +0100

    adding static to pythonanywhere

--- userlog/userlog/settings.py
+++ userlog/userlog/settings.py
@@ -131,8 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS= [STATIC_DIR,]
+STATICFILES_DIRS= [STATIC_DIR,"/home/threebros/myproj/static"]
 
 #MEDIA_DIR
 MEDIA_ROOT = [MEDIA_DIR,]
